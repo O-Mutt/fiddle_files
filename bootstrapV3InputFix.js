ef6a8ff189453d6edeb355768a07ff2cb32e7980
Matt Erickson <Matthew.Erickson@hbc.com>
2013-11-18 15:44:51 -0600

    Add input fix

---
+++ bootstrapV3InputFix.js
@@ -0,0 +1,135 @@
+﻿//This fixes an issue with bootstrap button selections
+//https://github.com/twbs/bootstrap/pull/10206
+//TODO: Remove after upgraded to Bootstrap Version 3.0.2 or greater
+
+
+$(document).off('click.bs.button.data-api', '[data-toggle^=button]');
+
+(function ($) {
+    "use strict";
+
+    // BUTTON PUBLIC CLASS DEFINITION
+    // ==============================
+
+    var Button = function (element, options) {
+        this.$element = $(element);
+        this.options = $.extend({}, Button.DEFAULTS, options);
+    };
+
+    Button.DEFAULTS = {
+        loadingText: 'loading...'
+    };
+
+    Button.prototype.setState = function (state) {
+        var d = 'disabled';
+        var $el = this.$element;
+        var val = $el.is('input') ? 'val' : 'html';
+        var data = $el.data();
+
+        state = state + 'Text';
+
+        if (!data.resetText) $el.data('resetText', $el[val]());
+
+        $el[val](data[state] || this.options[state]);
+
+        // push to event loop to allow forms to submit
+        setTimeout(function () {
+            (state === 'loadingText') ? $el.addClass(d).attr(d, d) : $el.removeClass(d).removeAttr(d);
+        }, 0);
+    };
+
+    Button.prototype.toggle = function () {
+        var $parent = this.$element.closest('[data-toggle="buttons"]');
+
+        if ($parent.length) {
+            var $input = this.$element.find('input');
+            if ($input.prop('type') === 'radio') {
+                $parent.find('.active').removeClass('active');
+            }
+
+            this.$element.toggleClass('active');
+            $input
+                .prop('checked', this.$element.hasClass('active'))
+                .trigger('change');
+        } else {
+            this.$element.toggleClass('active');
+        }
+    };
+
+
+    // BUTTON PLUGIN DEFINITION
+    // ========================
+
+    var old = $.fn.button;
+
+    $.fn.button = function (option) {
+        return this.each(function () {
+            var $this = $(this);
+            var data = $this.data('bs.button');
+            var options = typeof option == 'object' && option;
+
+            if (!data) $this.data('bs.button', (data = new Button(this, options)));
+
+            if (option == 'toggle') {
+                data.toggle();
+            } else if (option) {
+                data.setState(option);
+            }
+        });
+    };
+
+    $.fn.button.Constructor = Button;
+
+
+    // BUTTON NO CONFLICT
+    // ==================
+
+    $.fn.button.noConflict = function () {
+        $.fn.button = old;
+        return this;
+    };
+
+    // BUTTON DATA-API
+    // ===============
+
+    $(document).on('click.bs.button.data-api', '[data-toggle^=button]', function(e) {
+        var $btn = $(e.target);
+        if (!$btn.hasClass('btn')) $btn = $btn.closest('.btn');
+        $btn.button('toggle');
+        e.preventDefault();
+    });
+
+}(window.jQuery));
+
+/*
+    By clicking the radio buttons several times you can see the checked property is correct,
+    and also the active class is added to the proper label before triggering the change event.
+*/
+//$('.btn-group').each(function () {
+//    var $inputs = $(this).find('input');
+//    $inputs.on('change', function () {
+//        $inputs.each(function () {
+//            var $parent = $(this).parent();
+//            console.log(
+//                $parent.index(),
+//                this.checked,
+//                $parent.hasClass('active')
+//            );
+//        });
+//    });
+//});
+
+
+$(function () {
+    $('.btn-group').each(function () {
+        var $inputs = $(this).find('input');
+        $inputs.each(function () {
+            var $parent = $(this).parent();
+            if ($(this).is(":checked")) {
+                if (!$parent.hasClass('active')) {
+                    $parent.addClass('active');
+                }
+            }
+        });
+    });
+});
