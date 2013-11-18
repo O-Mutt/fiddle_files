77bb9a3a71f9c77a10d7a812be8e07008ed16fa8
Matt Erickson <Matthew.Erickson@hbc.com>
2013-11-18 08:45:57 -0600

    add unobtrusive

---
+++ jquery.validate.unbtrusive-support-for-bootstrap.js
@@ -0,0 +1,54 @@
+﻿//http://stackoverflow.com/questions/10217175/mvc-twitter-bootstrap-unobtrusive-error-handling
+$(function () {
+    //$('span.field-validation-valid, span.field-validation-error').each(function () {
+    //    $(this).addClass('help-inline');
+    //});
+
+    $('form').submit(function () {
+        if ($(this).valid()) {
+            $(this).find('div.form-group').each(function () {
+                if ($(this).find('span.field-validation-error').length == 0) {
+                    $(this).removeClass('error');
+                    $(this).removeClass('has-error');
+                }
+            });
+        }
+        else {
+            $(this).find('div.form-group').each(function () {
+                if ($(this).find('span.field-validation-error').length > 0) {
+                    $(this).addClass('error');
+                    $(this).addClass('has-error');
+                }
+            });
+        }
+    });
+    
+    $('form').each(function () {
+        $(this).find('div.form-group').each(function () {
+            if ($(this).find('span.field-validation-error').length > 0) {
+                $(this).addClass('error');
+                $(this).addClass('has-error');
+            }
+        });
+    });
+});
+
+//Update that validator
+$.validator.setDefaults({
+    highlight: function (element) {
+        console.log("call highlight");
+        $(element).closest(".form-group").addClass("error").addClass("has-error");
+    },
+    unhighlight: function (element) {
+        console.log("call unhighlight");
+        $(element).closest(".form-group").removeClass("error").removeClass("has-error");
+    },
+    onclick: function (one, two) {
+        console.log(one);
+        console.log(two);
+    },
+    onblur: function(one, two) {
+        console.log(one);
+        console.log(two);
+    }
+});
